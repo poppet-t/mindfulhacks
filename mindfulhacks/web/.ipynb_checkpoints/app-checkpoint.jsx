c265727f1f77489fb2404138b0818309b75b08d5
Amir Rahman <19.amir.rahman@gmail.com>
2022-09-06 20:28:47 +0800

    updated

---
+++ mindfulhacks/web/.ipynb_checkpoints/app-checkpoint.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import './big-screen.styles.css'
+export const BigScreen = () => {
+    return (
+        <div className="big-screen">
+        <h1>hellop</h1>
+        </div>
+    )
+}
+const element = <h1>Hello, world</h1>;
+root.render(element);
